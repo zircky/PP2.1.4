b3476da4c13efa58ab9d68132732340a6b727e9d
zircky <gfgfgfggf02@gmail.com>
2024-06-15 11:21:02 +0300

    comm

--- src/main/java/zi/zircky/demo2/model/AnimalsCage.java
+++ src/main/java/zi/zircky/demo2/model/AnimalsCage.java
@@ -10,19 +10,15 @@ public class AnimalsCage {
   @Autowired
   @Qualifier("dog")
   private Animal animal;
-  @Autowired
-  private Timer timer = new Timer();
 
-  public Timer getTimer() {
-    return timer;
-  }
+  @Autowired
+  private Timer timer;
 
   public void whatAnimalSay() {
     System.out.println("Say:");
     System.out.println(animal.toString());
     System.out.println("At:");
-
-    System.out.println(new Timer().getTime());
+    System.out.println(timer.getTime());
     System.out.println("________________________");
   }
 }
